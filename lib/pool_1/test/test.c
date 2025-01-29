985ff1b64a779c974dfb70259074c632dd140fc4
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-01-29 08:53:52 -0300

    test 1

--- lib/pool_1/test/test.c
+++ lib/pool_1/test/test.c
@@ -4,56 +4,56 @@
 
 
 
-void test_pool_palloc()
+void test_pool_1_palloc()
 {
     printf("\n\t%s", __func__);
 
-    handler_p h = palloc();
+    handler_p h = palloc_1();
     assert(h != NULL);
-    assert(pool_global.h == NULL);
+    assert(pool_1_global.h == NULL);
     free(h);
-    pool_clean();
+    pool_1_clean();
 
     assert(clu_mem_empty());
 }
 
-void test_pool_pfree(bool show)
+void test_pool_1_pfree(bool show)
 {
     printf("\n\t%s", __func__);
 
     if(show) printf("\n\t\t%s 1", __func__);
-    handler_p h1 = palloc();
-    pfree(h1);
-    assert(pool_global.h == h1);
-    assert(NEXT(pool_global.h) == NULL);
+    handler_p h1 = palloc_1();
+    pfree_1(h1);
+    assert(pool_1_global.h == h1);
+    assert(NEXT(pool_1_global.h) == NULL);
 
     if(show) printf("\n\t\t%s 2", __func__);
-    handler_p h2 = palloc();
+    handler_p h2 = palloc_1();
     assert(h2 == h1);
-    assert(pool_global.h == NULL);
+    assert(pool_1_global.h == NULL);
 
     if(show) printf("\n\t\t%s 3", __func__);
-    h2 = palloc();
+    h2 = palloc_1();
     assert(h2 != h1);
 
-    pfree(h2);
-    pfree(h1);
-    assert(pool_global.h == h1);
-    assert(NEXT(pool_global.h) == h2);
-    assert(NEXT(NEXT(pool_global.h)) == NULL);
-    pool_clean();
+    pfree_1(h2);
+    pfree_1(h1);
+    assert(pool_1_global.h == h1);
+    assert(NEXT(pool_1_global.h) == h2);
+    assert(NEXT(NEXT(pool_1_global.h)) == NULL);
+    pool_1_clean();
 
     assert(clu_mem_empty());
 }
 
 
 
-void test_pool()
+void test_pool_1()
 {
     printf("\n%s", __func__);
 
-    test_pool_palloc();
-    test_pool_pfree(false);
+    test_pool_1_palloc();
+    test_pool_1_pfree(false);
 
     assert(clu_mem_empty());
 }
@@ -62,8 +62,8 @@ void test_pool()
 int main() 
 {
     setbuf(stdout, NULL);
-    pool_intialize(8);
-    test_pool();
+    pool_1_intialize(8);
+    test_pool_1();
     printf("\n\n\tTest successful\n\n");
     return 0;
 }
