7f360385d91bff9cdaabcdf8d64685c67ca578be
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-01-29 12:29:08 -0300

    testing

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <time.h>
 
 #include "../lib/pool_1/header.h"
+#include "../lib/pool_2/header.h"
 #include "../utils/assert.h"
 
 STRUCT(keep)
@@ -22,6 +23,7 @@ keep_p keep_create(handler_p h, keep_p next)
     return k;
 }
 
+
 typedef handler_p (*alloc_t)();
 typedef void (*free_t)(handler_p);
 
@@ -30,17 +32,30 @@ handler_p my_malloc()
     return malloc(8);
 }
 
-void test(long lim, alloc_t f_alloc, free_t f_free)
+double my_time()
 {
+    struct timespec now;
+    timespec_get(&now, TIME_UTC);
+    return now.tv_sec + now.tv_nsec*1e-9;
+}
+
+keep_p test(char *tag, alloc_t f_alloc, free_t f_free)
+{
+    long lim = 1e10;
+    long target = 2e8;
+    int ceiling = 1e6;
+
+    printf("\n");
     long n = 0;
     keep_p k = NULL;
     for(long i=0; i<lim; i++)
     {
-        float p = (rand() % (int)1e6) / 1e6;
-        float threshold = 0.6 - 0.2 * n / 1e9;
+        float p = (rand() % ceiling) / (float)ceiling;
+        float threshold = 0.6 - 0.2 * n / target;
 
-        if(i%(long)1e7 == 0)
-            printf("\n%ld %ld %.2f", i/(long)1e7, n, threshold);
+        long interval = 1e7;
+        if(i%interval == 0)
+            printf("\n%s %ld/%ld %ld %.2f", tag, i/interval, lim/interval, n, threshold);
 
         if(n == 0 || (p < threshold))
         {
@@ -63,6 +78,19 @@ void test(long lim, alloc_t f_alloc, free_t f_free)
         k = k_next;
         n--;
     }
+    return k;
+}
+
+void keep_free(keep_p k)
+{
+    while(k)
+    {
+        free(k->h);
+        
+        keep_p k_aux = k->next;
+        free(k);
+        k = k_aux;
+    }
 }
 
 int main(int argc, char** argv)
@@ -71,23 +99,41 @@ int main(int argc, char** argv)
     printf("\nTemplate repo");
 
     srand(time(NULL));
-    // pool_1_intialize(8, 1e6);
+
+    double start, end;
+    keep_p k;
+
+    start = my_time();
+    k = test("malloc", my_malloc, free);
+    end = my_time();
+    keep_free(k);
+    double time_malloc = end - start;
+
     pool_1_intialize(8);
-    // test(1e10, palloc, pfree);
-    test(1e10, my_malloc, free);
+    start = my_time();
+    k = test("palloc_1", palloc_1, pfree_1);
+    end = my_time();
+    pool_1_clean();
+    keep_free(k);
+    double time_palloc_1 = end - start;
+
+    pool_2_intialize(8, 1e6);
+    start = my_time();
+    k = test("palloc_2", palloc_2, pfree_2);
+    end = my_time();
+    pool_2_clean();
+    keep_free(k);
+    double time_palloc_2 = end - start;
+
+    printf("\n");
+    printf("\ntime malloc  : %.3f", time_malloc);
+    printf("\ntime palloc 1: %.3f", time_palloc_1);
+    printf("\ntime palloc 2: %.3f", time_palloc_2);
     
     printf("\n");
     return 0;
 }
 
-// 30.133
-// 39.832
-
-// 82.98s
-// 1:54.89
-
-// 5:30.24
-// 8:40.08
-
-// 5:27.24
-
+// time malloc  : 325.594
+// time palloc 1: 294.143
+// time palloc 2: 290.207
